d98a0c998a31eb33e79a3aa4fac7897ef75984c7
Ahti Legonkov <ahti.legonkov@gmail.com>
2023-05-04 14:34:36 +0300

    Refactor + keep cursor in the middle of screen during scroll

--- lua/lego/options.lua
+++ lua/lego/options.lua
@@ -1,15 +1,15 @@
 vim.g.loaded_netrw = 1
 vim.g.loaded_netrwPlugin = 1
 
-
-vim.api.nvim_command('set tabstop=4')
-vim.api.nvim_command('set shiftwidth=4')
+vim.opt.scrolloff = 999
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4
 vim.api.nvim_command('set expandtab')
 vim.api.nvim_command('set nowrap')
 vim.api.nvim_command('set number')
-vim.api.nvim_command('set numberwidth=5')
+vim.opt.numberwidth = 5
 
-vim.api.nvim_command('set cursorline')
+vim.opt.cursorline = true
 vim.api.nvim_command('colorscheme habamax')
 -- vim.api.nvim_command('hi clear CursorLine')
 -- vim.api.nvim_command('hi clear CursorLineNr')
@@ -19,6 +19,6 @@ vim.api.nvim_command('colorscheme habamax')
 vim.api.nvim_command('hi WinSeparator guibg=NONE ctermbg=NONE')
 vim.api.nvim_command('hi Normal guibg=NONE ctermbg=NONE')
 -- Override ugly highlight for some cmake's keywords
-vim.api.nvim_command('hi ModeMsg guibg=NONE ctermbg=NONE ctermfg=173 guifg=#d7875f')
+-- vim.api.nvim_command('hi ModeMsg guibg=NONE ctermbg=NONE ctermfg=173 guifg=#d7875f')
 
 vim.api.nvim_command('set guifont=Cascadia\\ Mono\\ ExtraLight:h10')
