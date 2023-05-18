6395b7d636aa6cedee77bfe0a0004e2fae0eb704
Ahti Legonkov <ahti.legonkov@gmail.com>
2023-05-18 16:15:16 +0300

    Show line numbers in telescope preview winow

--- lua/lego/telescope.lua
+++ lua/lego/telescope.lua
@@ -4,21 +4,24 @@ require('telescope').setup {
 --     extensions = {}
 }
 
+-- Show line numbers in telescope preview window
+vim.cmd "autocmd User TelescopePreviewerLoaded setlocal number"
+
 -- Telescope
 local opts = function(desc)
-  return { desc = desc, noremap = true, silent = true }
+  return { desc = 'Telescope: '..desc, noremap = true, silent = true }
 end
 local builtin = require('telescope.builtin')
 local keymap_fn = vim.keymap.set
-keymap_fn('n', '<leader>tr', builtin.resume,       opts 'Telescope: resume')
+keymap_fn('n', '<leader>tr', builtin.resume,       opts 'resume')
 
-keymap_fn('n', '<leader>tf', builtin.find_files,   opts 'Telescope: find files')
-keymap_fn('n', '<leader>tg', builtin.live_grep,    opts 'Telescope: live grep')
-keymap_fn('n', '<leader>ts', builtin.grep_string,  opts 'Telescope: grep string under cursor')
-keymap_fn('n', '<leader>tb', builtin.buffers,      opts 'Telescope: buffers')
-keymap_fn('n', '<leader>tk', builtin.keymaps,      opts 'Telescope: keymaps')
--- keymap('n', '<leader>fh', builtin.help_tags, opts 'Telescope: help tags')
+keymap_fn('n', '<leader>tf', builtin.find_files,   opts 'find files')
+keymap_fn('n', '<leader>tg', builtin.live_grep,    opts 'live grep')
+keymap_fn('n', '<leader>ts', builtin.grep_string,  opts 'grep string under cursor')
+keymap_fn('n', '<leader>tb', builtin.buffers,      opts 'buffers')
+keymap_fn('n', '<leader>tk', builtin.keymaps,      opts 'keymaps')
+-- keymap('n', '<leader>fh', builtin.help_tags, opts 'help tags')
 
-keymap_fn('n', '<leader>gf', builtin.git_files,    opts 'Telescope: git files')
-keymap_fn('n', '<leader>gs', builtin.git_stash,    opts 'Telescope: git stashes')
-keymap_fn('n', '<leader>gb', builtin.git_branches, opts 'Telescope: git branches')
+keymap_fn('n', '<leader>gf', builtin.git_files,    opts 'git files')
+keymap_fn('n', '<leader>gs', builtin.git_stash,    opts 'git stashes')
+keymap_fn('n', '<leader>gb', builtin.git_branches, opts 'git branches')
