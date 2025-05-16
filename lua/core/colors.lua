f291b2b22e183a9836ccc8081e02d9be0799b0ed
Ahti Legonkov <ahtilego@microsoft.com>
2025-05-16 14:14:55 +0300

    Use Catppuccin mocha theme

--- lua/core/colors.lua
+++ lua/core/colors.lua
@@ -1,5 +1,6 @@
 vim.opt.termguicolors = true
-vim.cmd 'colorscheme habamax'
+-- vim.cmd 'colorscheme habamax'
+vim.cmd 'colorscheme catppuccin-mocha'
 vim.api.nvim_command('hi WinSeparator guibg=NONE ctermbg=NONE')
 -- vim.api.nvim_command('hi Normal guibg=NONE ctermbg=NONE')
 
